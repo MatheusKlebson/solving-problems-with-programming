8c2e468438ee77013cac1153a43873a219574f4a
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-12-23 01:00:02 -0200

    Problem05

--- EstruturaSequencial/Problem05.py
+++ EstruturaSequencial/Problem05.py
@@ -1,4 +1,3 @@
-number_one = int(input("First number: "))
-number_two = int(input("Second number: "))
-sum_numbers = number_one + number_two
-print(f"The sum between {number_one} and {number_two} = {sum_numbers}")
+meters = int(input("Meters (M): "))
+conversation = meters * 100
+print(f"Result: {conversation}cm")

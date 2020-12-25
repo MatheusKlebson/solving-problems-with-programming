7be6631544b8218cbc5d86795d648f37259b0ae2
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-12-25 13:16:44 -0200

    Solution problem06

---
+++ EstruturaSequencial/Problem05 copy.py
@@ -0,0 +1,3 @@
+meters = int(input("Meters (M): "))
+conversation = meters * 100
+print(f"Result: {conversation}cm")
---
+++ EstruturaSequencial/Problem06.py
@@ -0,0 +1,5 @@
+'''Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+'''
+raio = float(input("Quanto vale o raio? (cm) "))
+area = 3.14*raio**2
+print(f"A area mede {area}cm2")
